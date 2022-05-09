2a2703b6ca96c5acfaa69da37c6a31a3007d19da
ProjektOpensource <69577313+ProjektOpensource@users.noreply.github.com>
2022-05-09 20:11:27 +0200

    added file to build and run the os (for now)

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd sec1
+nasm boot.asm -f bin -o boot.bin
+cd ..
+cd sec2
+nasm extended.asm -f bin -o extended.bin
+cd ..
+copy /b sec1\boot.bin+sec2\extended.bin test.flp
+
+qemu-system-x86_64 test.flp
